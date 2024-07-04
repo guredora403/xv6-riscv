648b120e7859cfeae7a73282cd21c1d2077d26c2
guredora <contact@guredora.com>
2024-07-04 16:07:41 +0900

    updated RTC access

--- kernel/rtc.c
+++ kernel/rtc.c
@@ -1,14 +1,19 @@
-#Include "types.h"
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "riscv.h"
+#include "defs.h"
 #include "rtc.h"
 
-uint32 rtc_read(uint32 offset){
-    return *(volatile uint32*)(GOLDFISH_RTC_BASE + offset);
+uint32 rtc_read(int offset){
+    volatile uint32 *ptr = (uint32*)(GOLDFISH_RTC0 + offset);
+    return *ptr;
 }
 
 uint64 gettime(void){
     uint32 low = rtc_read(RTC_TIME_LOW);
     uint32 high = rtc_read(RTC_TIME_HIGH);
-    return ((uint64)high << 32) | low;
+    return (((uint64)high << 32) | low) / 1000000000;
 }
 
 uint64 sys_gettime(void){
--- kernel/rtc.h
+++ kernel/rtc.h
@@ -2,13 +2,11 @@
 
 #include "types.h"
 
-// base address
-#define GOLDFISH_RTC_BASE 0x101000
 
 //data offsets
 #define RTC_TIME_LOW     0x00
 #define RTC_TIME_HIGH    0x04
 
 
-uint32 rtc_read(uint32 offset);
+uint32 rtc_read(int offset);
 uint64 gettime(void);
